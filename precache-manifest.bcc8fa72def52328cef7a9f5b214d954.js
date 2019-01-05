b23b0224d86041916cc5377cca17ca225f17111b
Kyle <rhodestraveler@gmail.com>
2019-01-04 22:41:32 -0600

    Updates

---
+++ precache-manifest.bcc8fa72def52328cef7a9f5b214d954.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "6dc79dba6bd021c0bf6b",
+    "url": "/myapp/static/js/main.6dc79dba.chunk.js"
+  },
+  {
+    "revision": "5d7ff01f848c9800f665",
+    "url": "/myapp/static/js/1.5d7ff01f.chunk.js"
+  },
+  {
+    "revision": "6dc79dba6bd021c0bf6b",
+    "url": "/myapp/static/css/main.cb32f319.chunk.css"
+  },
+  {
+    "revision": "e82700ee6ca92dd39d402b6fddac7147",
+    "url": "/myapp/index.html"
+  }
+];
