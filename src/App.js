69f2e30b8c85a61d38da843c0846a2e1442a8e8f
Kyle <rhodestraveler@gmail.com>
2019-01-04 21:17:06 -0600

    commiting changes after new git init in proper directory

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home";
+import Resume from "./Resume";
+import Contact from "./Contact";
+import Nav from "./Nav";
+import Projects from "./Projects";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Route exact path="/" component={Home} />
+          <Route path="/resume" component={Resume} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/projects" component={Projects} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/Nav.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default class Nav extends React.Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/contact/">Contact</Link>
+          </li>
+          <li>
+            <Link to="/resume/">Resume</Link>
+          </li>
+          <li>
+            <Link to="/projects/">Projects</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
